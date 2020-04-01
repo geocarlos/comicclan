fdfea41a1954473b843374276c94311e6295d7e3
Geocarlos Alves <geocarlos.alves@pixida.com.br>
2020-03-31 22:23:57 -0300

    Attribute value to initial state

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -2,7 +2,7 @@ import { combineReducers, Reducer } from 'redux';
 import keys from '../actions/ActionTypeKeys';
 import ActionTypes from '../actions/ActionTypes';
 
-const books: Reducer<any, ActionTypes> = (state: [], action: ActionTypes) => {
+const books: Reducer<any, ActionTypes> = (state = [], action: ActionTypes) => {
 	switch(action.type) {
 		case keys.GET_BOOKS:
 			return action.books;
