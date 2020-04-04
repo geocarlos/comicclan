b8603db29b8e017ae1b3d9c70bdaa8e668944ec9
Geocarlos Alves <geocarlos.alves@pixida.com.br>
2020-04-04 11:54:06 -0300

    Create models

---
+++ src/models/IBook.ts
@@ -0,0 +1,13 @@
+interface IBook {
+	name: string;
+	writer: string;
+	artist: string;
+	publication: string
+	owner: string
+	rating: number
+	image: string
+	summary: string
+	year: number
+}
+
+export default IBook;
